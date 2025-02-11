3baee714dbe83f8be165b9f52bdd6c5f30505b24
chris wren <chriswrendeveloper@gmail.com>
2025-02-11 10:53:21 +0000

    Added state to collapse sidebar

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,15 +1,32 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
   return (
-    <div>
+    <div className={sidebarClassNames}>
       {/* TOP LOGO */}
       <div
-        className={`flex gap-3 justify-between md:justify-normal items-center pt-8`}
+        className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSidebarCollapsed ? "px-5" : "px-8"
+        }`}
       >
         <Image
           src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
@@ -18,11 +35,17 @@ const Sidebar = () => {
           height={27}
           className="rounded w-8"
         />
-        <h1 className={`font-extrabold text-2xl`}>CWSTOCK</h1>
+        <h1
+          className={`${
+            isSidebarCollapsed ? "hidden" : "block"
+          } font-extrabold text-2xl`}
+        >
+          CWSTOCK
+        </h1>
 
         <button
           className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
@@ -32,7 +55,7 @@ const Sidebar = () => {
       <div className="flex-grow mt-8">{/* links here */}</div>
 
       {/* FOOTER */}
-      <div className={`mb-10`}>
+      <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
         <p className="text-center text-xs text-gray-500">&copy; 2025 CWstock</p>
       </div>
     </div>
