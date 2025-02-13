d515fdb9e5983078783f7313a228d7554ccab96e
chris wren <chriswrendeveloper@gmail.com>
2025-02-13 11:42:19 +0000

    Added Products controller and routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,10 @@ import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -20,6 +22,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
